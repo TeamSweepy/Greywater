3665e8fa9e1d30772e94f749be5ef4866a81c83e
Jeremy Barnes <jeremy-barnes@utulsa.edu>
2014-01-10 23:46:12 -0600

    Initial Commit - created libGDX project, no Sweepy code yet.

---
+++ Greywater-desktop/src/com/teamsweepy/greywater/Main.java
@@ -0,0 +1,16 @@
+package com.teamsweepy.greywater;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Greywater";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new Engine(), cfg);
+	}
+}
