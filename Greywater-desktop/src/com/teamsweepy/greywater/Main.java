02aaa2ddff5adfb391b7542f9ad2bd161bed7ca8
Jeremy Barnes <jeremy-barnes@utulsa.edu>
2014-01-14 00:09:47 -0600

    Set up basic scaling, camera, and viewport.

--- Greywater-desktop/src/com/teamsweepy/greywater/Main.java
+++ Greywater-desktop/src/com/teamsweepy/greywater/Main.java
@@ -1,16 +1,25 @@
+/**
+ * This is just an entry point for desktop applications, it sets the window 
+ * to the user's current resolution and makes the window fullscreen and then launches the game.
+ * 
+ * Important note - this is what you need to run in Eclipse to make the project actually go.
+ */
+
 package com.teamsweepy.greywater;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class Main {
+
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "Greywater";
 		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.resizable = false;
 		
+		//set fullscreen and match current resolution
+		cfg.setFromDisplayMode(LwjglApplicationConfiguration.getDesktopDisplayMode());
 		new LwjglApplication(new Engine(), cfg);
 	}
 }
--- Greywater/src/com/teamsweepy/greywater/Engine.java
+++ Greywater/src/com/teamsweepy/greywater/Engine.java
@@ -1,3 +1,7 @@
+/**
+ * This drives the rest of the game.
+ * 
+ */
 package com.teamsweepy.greywater;
 
 import com.badlogic.gdx.ApplicationListener;
@@ -8,59 +12,84 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
 
 public class Engine implements ApplicationListener {
+
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
+
+	public static final int NATIVE_WIDTH = 1600;
+	public static final int NATIVE_HEIGHT = 900;
+	public static final double NATIVE_ASPECT_RATIO = 16.0 / 9.0;
+
+
+	/* *********** BULLSHIT TESTING VARIABLES ************* */
 	private Texture texture;
 	private Sprite sprite;
-	
+	private Sprite bork;
+
+
+	/**
+	 * Initialize core assets, automatically called by LibGDX
+	 */
 	@Override
-	public void create() {		
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
-		
-		camera = new OrthographicCamera(1, h/w);
+	public void create() {
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, NATIVE_WIDTH, NATIVE_HEIGHT);
+
 		batch = new SpriteBatch();
-		
-		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
+		Texture.setEnforcePotImages(false); //binary texture sizes are for the 80's
+
+		//set up bullshit testing variables TODO remove
+		texture = new Texture(Gdx.files.internal("data/tavish_stand_south.png"));
+		sprite = new Sprite(texture);
 		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
-		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		
-		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+		bork = new Sprite(new Texture(Gdx.files.internal("data/bork.png")));
 	}
 
+	/**
+	 * Dispose of unmanaged assets such as the spritebatch and all textures
+	 */
 	@Override
 	public void dispose() {
 		batch.dispose();
 		texture.dispose();
 	}
 
+	/**
+	 * Render is called 60 times per second by LibGDX.
+	 * Render should call our own Render method that can be skipped if the system is lagging
+	 * and it should also call the physics/logic update method, and never skip
+	 */
 	@Override
-	public void render() {		
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+	public void render() {
+		camera.update();
+		camera.apply(Gdx.gl10);
+
+		//scale from 1600x900 to whatever user screen is set to
+		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		
-		batch.setProjectionMatrix(camera.combined);
+		batch.setProjectionMatrix(camera.combined); //openGL magic
+		Gdx.gl.glClearColor(0, 0, 0, 1); //black background
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+
+
+		//primary rendering area
 		batch.begin();
-		sprite.draw(batch);
+		//TODO remove drawing BS images
+		batch.draw(bork, 0, 0);
+		batch.draw(sprite, 0, 0);
 		batch.end();
 	}
 
 	@Override
-	public void resize(int width, int height) {
-	}
+	public void resize(int width, int height) {}
 
 	@Override
-	public void pause() {
-	}
+	public void pause() {}
 
 	@Override
-	public void resume() {
-	}
+	public void resume() {}
 }
