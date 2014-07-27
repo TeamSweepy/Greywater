849dadbcf6254157a10deb7c47d9a90051d5cfb0
biodiscus <robin@biodiscus.net>
2014-07-27 15:28:18 +0200

    Added the correct copyright comment

--- Greywater/src/com/teamsweepy/greywater/utils/Mathf.java
+++ Greywater/src/com/teamsweepy/greywater/utils/Mathf.java
@@ -1,7 +1,8 @@
 package com.teamsweepy.greywater.utils;
 
 /**
- * © Biodiscus.net, Robin de Jong
+ * Copyright Team Sweepy - Robin de Jong 2014 All use outside of the Greywater Project is not permitted unless express permission is
+ * granted. Email TeamSweepy@gmail.com to discuss usage.
  */
 public class Mathf {
     public static final float ROOT_TWO = (float)Math.sqrt(2);
